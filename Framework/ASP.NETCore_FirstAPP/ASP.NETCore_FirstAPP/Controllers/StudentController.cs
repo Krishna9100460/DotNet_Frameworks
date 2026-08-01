2751f9c41b1a436c8181527d36fd8bf4dc43f2b4
KrishnaGrl <310597670+Krishna9100460@users.noreply.github.com>
2026-08-01 19:11:30 +0530

    new project

---
+++ Framework/ASP.NETCore_FirstAPP/ASP.NETCore_FirstAPP/Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using ASP.NETCore_FirstAPP.Model;
+
+namespace ASP.NETCore_FirstAPP.Controllers
+{
+    public class StudentController
+    {
+        // List to store student information>
+        public List<Student> students = new List<Student>();
+        public void GetStudent()
+        {
+            // Implementation for getting student information
+        }
+
+        public void GetStudentId()
+        {
+            // Implementation for getting student by ID
+        }
+
+        public void AddStudent()
+        {
+            // Implementation for adding a new student
+        }
+
+
+    }
+}
---
+++ Framework/ASP.NETCore_FirstAPP/ASP.NETCore_FirstAPP/Model/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace ASP.NETCore_FirstAPP.Model
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+    }
+}
